76e9189a0124dd0bf670d56e3ca36d926cf83538
eboselea <eboselea@berea.edu>
2025-04-05 15:33:37 -0400

    Refined the PlayerMovement class with the attributes x, y, and speed, which control the player's x position, y position, and the speed of their character. Also created a method called player_position which can retrieve the players current position using the current coordinates. I then created an onject called player 1 and used it with the class. Then I called player_up using the player1 object, and printed the player's position.

--- Final-Project-codebase/Final Code/Class Conversion Process.py
+++ Final-Project-codebase/Final Code/Class Conversion Process.py
@@ -1,14 +1,28 @@
 # Create the CRC card.
 class PlayerMovement:
-    def __init__(self, player_name, player_move): # What is this? | In this case, a method called __init__ is being defined, in which the paren. rep. attributes that go into the function. For example, f(x) - a function is a label that takes x as an input, and generate an output.
-        self.player_name = player_name
-        self.player_move = player_move
+    def __init__(self, x=0, y=0, speed=0):# What is this? | In this case, a method called __init__ is being defined, in which the paren. rep. attributes that go into the function. For example, f(x) - a function is a label that takes x as an input, and generate an output.
+        self.x = x
+        self.y = y
+        self.speed = speed
     # GOAL: Abstract the functions for player movement (up,down,left,right) into methods that can be used as part of this class.
+
     def player_up(self):
-        return self.player_name + " is moving up."
+         self.y += self.speed
+
     def player_down(self):
-        return self.player_name + " is moving down."
+         self.y -= self.speed
+
     def player_left(self):
-        return self.player_name + " is moving left."
+         self.x += self.speed
+
     def player_right(self):
-        return self.player_name + " is moving right."
+         self.y -= self.speed
+
+    def player_position(self):
+        return(self.x, self.y)
+
+player1 = PlayerMovement()
+player1.player_up()
+print(player1.player_position)
+
+
